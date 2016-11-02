dddda27f7b9dff83dc23d7871d4c8015bcba8d81
sdowell <sdowell@umail.ucsb.edu>
2016-11-02 11:50:18 -0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -3,11 +3,36 @@ def parseBandwidth():
   pass
 
 def parsePacketLoss(filename):
+  tuples = [[0,0],[0,0],[0,0]]
   with open(filename) as f:
     lines = f.readlines()
   for line in lines:
-     print line
-  return
+    words = line.split()
+    index = 0
+    while index < len(words):
+      event = words[0]
+      source = int(words[4])
+      dest = int(words[6])
+      protocol = words[8]
+      size = int(words[10])
+      flow = int(words[12])
+      if source == 0 && event == "-":
+        tuples[flow][0]++
+      if event == "d":
+        dropped[flow][1]++
+      continue
+      
+      flow = -1
+      if words[index] == "-c":
+        index++
+        flow = int(words[index])
+        if words[0] == "d":
+          dropped[flow]++
+        elif words[0] == "-"
+      index++
+      
+      
+  return tuples
 
 
 def main():
