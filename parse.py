3f1d4372ed4e4d278e0cc29de3e1cfa63a2d93cb
sdowell <sdowell@umail.ucsb.edu>
2016-11-02 12:13:38 -0700

    Update parse.py
    Implemented bandwidth parser

--- parse.py
+++ parse.py
@@ -1,6 +1,25 @@
 
-def parseBandwidth():
-  pass
+def parseBandwidth(filename):
+  bws = [0,0,0]
+  with open(filename) as f:
+    lines = f.readlines()
+  for line in lines:
+    words = line.split()
+    if words[2] == "*":
+      continue
+    event = words[0]
+    source = int(words[4])
+    dest = int(words[6])
+    protocol = words[8]
+    size = int(words[10])
+    flow = int(words[12])
+    if source == 0 and flow == 1 and event == "-":
+      bws[flow] += size
+    if source == 4 and flow == 2 and event == "-":
+      bws[flow] += size
+    if source == 1 and flow == 0 and event == "-":
+      bws[flow] += size
+  return tuples
 
 def parsePacketLoss(filename):
   tuples = [[0,0],[0,0],[0,0]]
